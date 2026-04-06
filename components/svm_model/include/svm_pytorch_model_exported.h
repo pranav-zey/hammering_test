b4fe78bc31aecfa6df9ab175bdb584ce932d3ac4
ManishGru <gurumanish2015@gmail.com>
2026-04-06 22:39:14 +0545

    added test svm model.

---
+++ components/svm_model/include/svm_pytorch_model_exported.h
@@ -0,0 +1,17 @@
+#ifndef __SVM_PYTORCH_MODEL_EXPORTED_H__
+#define __SVM_PYTORCH_MODEL_EXPORTED_H__
+
+#include<stdint.h>
+
+#define INPUT_SIZE 48
+
+static const int8_t WEIGHTS[INPUT_SIZE] = {
+-5, -55, -127, -11, 59, 94, 80, -19, 92, 28, 8, -2, 43, 22, -41, 6, -33, 32, 22, -1, 10, 37, -5, 36, -37, 31, -48, -72, 82, -42, -23, -4, 7, -67, 15, 5, 7, 37, 10, 20, -2, 11, 19, -12, -1, -4, 26, -4
+};
+
+static const int32_t BIAS = -97;
+
+static const float W_SCALE = 0.0034774565137922764f;
+static const float X_SCALE = 0.13599954545497894f;
+
+#endif
